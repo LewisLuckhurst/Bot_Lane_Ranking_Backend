cf4ad4b8f36f74fa47ad6e810be833f73740da5a
Lewis Luckhurst <lewisluckhurst@icloud.com>
2020-10-16 17:46:53 +0100

    Creating the project structure

---
+++ src/main/java/com/botlaneranking/www/BotLaneRankingBackend/BotLaneRankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.botlaneranking.www.BotLaneRankingBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotLaneRankingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotLaneRankingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botlaneranking/www/BotLaneRankingBackend/BotLaneRankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botlaneranking.www.BotLaneRankingBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotLaneRankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
